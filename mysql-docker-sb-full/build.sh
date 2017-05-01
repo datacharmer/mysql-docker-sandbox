597f74300ff46a79992c46f623434293fe292919
Giuseppe Maxia <g.maxia@gmail.com>
2017-05-01 10:23:16 +0200

    removed binary files

--- mysql-docker-sb-full/build.sh
+++ mysql-docker-sb-full/build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+cd $(dirname $0)
+binaries=../../mysql-docker-minimal/dbdata
+if [ ! -d $binaries ]
+then
+    echo "$binaries not found"
+    exit 1
+fi
+cd $binaries
+binaries=$PWD
+cd -
+#echo $binaries
 cp Dockerfile.small Dockerfile
 IMAGE=datacharmer/mysql-sb-gz
 docker build  -t $IMAGE:latest .
@@ -7,11 +18,11 @@ cp Dockerfile.full Dockerfile
 IMAGE=datacharmer/mysql-sb-full
 
 cd dbdata
-for D in ../dbdata-gz/*.gz
+for D in $binaries/*.gz
 do
     tar -xzf $D
 done
 cd ..
 docker build  -t $IMAGE:latest .
 
-rm -rf dbdata/5*
+rm -rf dbdata/[58]*
