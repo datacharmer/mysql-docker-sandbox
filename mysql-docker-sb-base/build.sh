514d7d49931af3ffa7d6411777f47eade83baf03
Giuseppe Maxia <g.maxia@gmail.com>
2018-04-20 11:52:13 +0200

    added dbdeployer to the base OS

--- mysql-docker-sb-base/build.sh
+++ mysql-docker-sb-base/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd $(dirname $0)
+cp $HOME/go/bin/dbdeployer .
+if [ "$?" != "0" ] ; then exit ; fi
+cp /etc/bash_completion.d/dbdeployer_completion.sh .
+if [ "$?" != "0" ] ; then exit ; fi
 docker build -t datacharmer/mysql-sb-base .
